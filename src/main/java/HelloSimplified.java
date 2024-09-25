0a7e106e50d57e97ba11f4a30d704ca061080aa4
Emanuele <egalli64@gmail.com>
2024-09-25 08:47:29 +0200

    clarify

--- src/main/java/HelloSimplified.java
+++ src/main/java/HelloSimplified.java
@@ -2,12 +2,13 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Hello Java by Unnamed class
- * 
- * Run it from CLI by: java --enable-preview HelloSimplified.java
  */
 
+/**
+ * Hello Java by Unnamed class
+ * <p>
+ * Run it from CLI by: java --enable-preview HelloSimplified.java
+ */
 void main() {
     System.out.println("Hello");
 }
