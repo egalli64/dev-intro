e5000a541d62950a599efebc2c76ea4e00cffe6e
Emanuele <egalli64@gmail.com>
2024-09-25 08:49:18 +0200

    clarify

--- src/main/java/M1_S2_X1.java
+++ src/main/java/M1_S2_X1.java
@@ -2,10 +2,11 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Exercises on boolean variables and operators
  */
 
+/**
+ * Exercise on boolean
+ */
 void main() {
     int target = -42;
     System.out.println("target is " + target);
