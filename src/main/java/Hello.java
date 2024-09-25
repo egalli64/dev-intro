d471eb1608bfc0c1d352a436f97df29729537c24
Emanuele <egalli64@gmail.com>
2024-09-25 08:46:04 +0200

    clarify

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,11 +2,11 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Hello Java
- * Run it from CLI by: java Hello.java
  */
 
+/**
+ * Hello Java - Run it from CLI by: java Hello.java
+ */
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
