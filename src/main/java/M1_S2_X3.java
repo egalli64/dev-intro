f69b1202043fb413d8a7a5f22b4cbfd0d5e88477
Emanuele <egalli64@gmail.com>
2024-09-25 08:50:15 +0200

    clarify

--- src/main/java/M1_S2_X3.java
+++ src/main/java/M1_S2_X3.java
@@ -2,10 +2,11 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Exercises on boolean variables and operators
  */
 
+/**
+ * Exercise on boolean
+ */
 void main() {
     int left = -42;
     int right = -18;
