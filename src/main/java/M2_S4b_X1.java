b5c0e01b78af9d1195cbc1b7e3e302cfb87828d3
Emanuele <egalli64@gmail.com>
2024-09-25 08:46:27 +0200

    exercise

---
+++ src/main/java/M2_S4b_X1.java
@@ -0,0 +1,17 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on if-else
+ */
+void main() {
+    int tomAge = 42;
+    System.out.println("Tom age is " + tomAge);
+
+    // TODO: underage should be true only if tom age is less than 18
+    boolean underage = true;
+    System.out.println("Is Tom underage? " + underage);
+}
