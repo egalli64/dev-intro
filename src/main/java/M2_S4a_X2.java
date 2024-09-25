0b7314b049f07047b3112589164789c3df640908
Emanuele <egalli64@gmail.com>
2024-09-25 08:34:05 +0200

    exercise

---
+++ src/main/java/M2_S4a_X2.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ *
+ * Exercise on division
+ */
+
+void main() {
+    int a = 12;
+    System.out.println("a is " + a);
+
+    int b = 8;
+    System.out.println("b is " + b);
+
+    // TODO: assign to c the result of the difference of a by b
+    int c = 0;
+    System.out.println("the difference between a and b is " + c);
+}
