0df7662536d24e911567270875fb93403e5aa33b
Emanuele <egalli64@gmail.com>
2024-09-26 08:19:37 +0200

    exercise

---
+++ src/main/java/M2_S4b_X2.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on if-else if-else
+ */
+void main() {
+    int number = 42;
+    System.out.println("The chosen number is " + number);
+
+    // TODO: check if the number is positive, negative, or zero
+    // just the correct answer should be printed
+    System.out.println("The number is positive");
+    System.out.println("The number is negative");
+    System.out.println("The number is zero");
+}
