58751d39ae7b7d12dba25e78c08fab1fe2f9e360
Emanuele <egalli64@gmail.com>
2024-09-27 08:57:28 +0200

    readability

---
+++ src/main/java/M2_S4d_X.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on do-while
+ */
+void main() {
+    int first = 1;
+    System.out.println("First value is " + first);
+
+    int last = 10;
+    System.out.println("Last value is " + last);
+
+    // TODO: using a while loop, print the odd values in [first .. last]
+    System.out.println("The odd values between first and last: ");
+}
