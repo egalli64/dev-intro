50aaa6e167f527609a89bb1a8d4010b35765c0e8
Emanuele <egalli64@gmail.com>
2024-09-28 19:14:09 +0200

    readability

--- src/main/java/M2_S4g_Continue.java
+++ src/main/java/M2_S4g_Continue.java
@@ -2,10 +2,11 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Jump to the next iteration in a loop with continue
  */
 
+/**
+ * Jump to the next iteration in a loop with continue
+ */
 void main() {
     for (int value = 0; value < 10; value++) {
         System.out.print(' ');
