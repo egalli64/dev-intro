aea475227f447d6d607c29e6fb7b0a5f97f595b0
Emanuele <egalli64@gmail.com>
2024-09-28 19:22:17 +0200

    exercise

---
+++ src/main/java/M2_S4g_X.java
@@ -0,0 +1,21 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on continue
+ */
+void main() {
+    int first = 1;
+    System.out.println("First value is " + first);
+
+    int last = 10;
+    System.out.println("Last value is " + last);
+
+    System.out.println("The even values between first and last: ");
+    for (int i = first; i <= last; i++) {
+        // TODO: use continue to skip the print of odd values
+    }
+}
