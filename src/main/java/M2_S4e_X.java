209ee8c4933b38d664328bb1b1577d62b81f61e2
Emanuele <egalli64@gmail.com>
2024-09-28 10:17:36 +0200

    exercise

---
+++ src/main/java/M2_S4e_X.java
@@ -0,0 +1,19 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on for loop
+ */
+void main() {
+    int first = 1;
+    System.out.println("First value is " + first);
+
+    int last = 10;
+    System.out.println("Last value is " + last);
+
+    // TODO: using a for loop, print the even values in [first .. last]
+    System.out.println("The even values between first and last: ");
+}
