97e0848a763da31c29dd01d473d99e9d6646a258
Emanuele <egalli64@gmail.com>
2024-09-28 10:42:13 +0200

    exercise

---
+++ src/main/java/M2_S4f_X.java
@@ -0,0 +1,15 @@
+/*
+ * Introduction to Software Development (Java)
+ * 
+ * https://github.com/egalli64/dev-intro
+ */
+
+/**
+ * Exercise on break for a loop
+ */
+void main() {
+    for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+        // TODO: interrupt the loop if the loop variable is equal to five
+    }
+}
