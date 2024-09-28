6c4a25aeb66cd645b53e843e27a5add54c60e5b7
Emanuele <egalli64@gmail.com>
2024-09-28 10:15:12 +0200

    clarify

--- src/main/java/M2_S4e_For.java
+++ src/main/java/M2_S4e_For.java
@@ -2,10 +2,11 @@
  * Introduction to Software Development (Java)
  * 
  * https://github.com/egalli64/dev-intro
- *
- * Loop for
  */
 
+/**
+ * Loop statement: for
+ */
 void main() {
     for (int value = 0; value < 10; value++) {
         System.out.println(value);
