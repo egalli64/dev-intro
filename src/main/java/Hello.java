d49c2926159a71a8d557680a38f8c3620ff26ac6
Emanuele <egalli64@gmail.com>
2024-11-26 16:28:24 +0100

    clarify

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,6 +9,7 @@
  */
 public class Hello {
     public static void main(String[] args) {
+        // print the string "Hello", then terminates the line
         System.out.println("Hello");
     }
 }
