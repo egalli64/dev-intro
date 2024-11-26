b685c806de6c3d640c937b9d7e54fc8687b63775
Emanuele <egalli64@gmail.com>
2024-11-26 17:25:34 +0100

    readability

--- src/main/java/M1_S2_X2.java
+++ src/main/java/M1_S2_X2.java
@@ -9,8 +9,9 @@
  */
 void main() {
     int left = -42;
-    int right = 18;
     System.out.println("left is " + left);
+
+    int right = 18;
     System.out.println("right is " + right);
 
     // TODO: set positive to true if both left and right are greater than zero
